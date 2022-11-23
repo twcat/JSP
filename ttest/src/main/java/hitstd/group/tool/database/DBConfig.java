e07a445efa906ad4fed665ca2611b2eb6013022a
AnnaLee1215 <ec460520@gmail.com>
2022-11-23 16:37:19 +0800

    Create DBConfig.java

---
+++ ttest/src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -0,0 +1,12 @@
+package hitstd.group.tool.database;
+
+public class DBConfig {
+	public DBConfig(){		
+	}
+	
+	public String FilePath(){
+	   //DB FilePath
+		 String DBPath="C:\\\\Program Files\\\\Apache Software Foundation\\\\Tomcat 9.0\\\\webapps\\\\ttest\\\\src\\\\main\\\\webapp\\\\member.accdb";
+	   return DBPath;		
+	}
+}
